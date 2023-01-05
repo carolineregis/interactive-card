c04725c0e3bac0994afefd7bcc1d9ec3a1a356f3
Carol Ayane <tiny.regis@gmail.com>
2023-01-04 23:50:59 -0300

    WIP(*): capturando dados digitados e imprimndo no cartão

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 let userName = '';
+let cardNumber = '';
+let expDate = '';
 let cardPlaceholderName = document.getElementById('cardName');
+let cardPlaceholderNumber = document.getElementById('cardNumber');
 
 function getUserName() {
     userName = document.getElementById('userName').value;
-    cardPlaceholderName.textContent = userName;
+    cardPlaceholderName.textContent = userName.toUpperCase();
+
+    if (!userName) {
+        cardPlaceholderName.textContent = 'MANOEL GOMES DO CANETA AZUL';
+    }
+}
+
+function getCardNumber() {
+    cardNumber = document.getElementById('userNumber').value;
+    let formattedCardNumber = cardNumber.replace(/(\d{4})/g, '$1 ');
+    cardPlaceholderNumber.textContent = formattedCardNumber;
+
+    if (!cardNumber) {
+        cardPlaceholderNumber.textContent = '0000 0000 0000 0000';
+    }
+}
+
+function getExpDate() {
+    expDate = document.getElementById('userNumber').value;
 }
 
